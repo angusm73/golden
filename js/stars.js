bfa0995c3340f6ba430067ed9ac194d5b2da5287
Angus Moore <angusmoore73@gmail.com>
2018-08-08 21:58:31 +1000

    Glowing stars

--- js/stars.js
+++ js/stars.js
@@ -1,27 +1,40 @@
-let max_stars = window.innerWidth * 1.5
+let max_stars = window.innerWidth * 0.5
 let stars = []
 let canvas_width = document.body.clientWidth
 let canvas_height = document.body.clientHeight
+let pg
 
 function setup() {
     createCanvas(canvas_width, canvas_height)
+
+    // Generate initial stars
     for (let i = 0; i < max_stars; i++) {
         stars.push(new Star)
     }
+
+    // Create star glow
+    pg = createGraphics(100, 100)
+    pg.background('rgba(0,0,0,0)')
+    pg.stroke(255)
+    pg.ellipse(50, 50, 25, 25)
+    pg.filter(BLUR, 10)
+    pg.loadPixels()
+    pg.updatePixels()
+    pg.filter(DILATE)
 }
 
 function windowResized() {
     canvas_width = document.body.clientWidth
     canvas_height = document.body.clientHeight
     resizeCanvas(canvas_width, canvas_height)
-    if (window.innerWidth * 1.5 > max_stars) {
+    if (window.innerWidth * 0.5 > max_stars) {
         for (let i = 0; i < max_stars; i++) {
             if (!stars[i].length) {
                 stars.push(new Star)
             }
         }
     }
-    max_stars = window.innerWidth * 1.5
+    max_stars = window.innerWidth * 0.5
     stars = stars.splice(0, max_stars)
 }
 
@@ -42,9 +55,9 @@ class Star {
     move() {
         const distance_to_cursor = dist(this.x, this.y, mouseX, mouseY)
         const multiplier = !mouseIsPressed
-            ? distance_to_cursor < 130 ? (150 - distance_to_cursor) * 0.04 : 1
+            ? distance_to_cursor < 160 ? (180 - distance_to_cursor) * 0.04 : 1
             : 1
-        this.tracer_length = distance_to_cursor < 150 ? (150 - distance_to_cursor) * 0.2 : 0
+        this.tracer_length = distance_to_cursor < 180 ? (180 - distance_to_cursor) * 0.2 : 0
         this.x += this.velocity.x * multiplier
         this.y += this.velocity.y * multiplier
         if (this.x < 0) {
@@ -59,6 +72,7 @@ class Star {
         }
     }
     draw() {
+        noStroke()
         if (this.tracer_length) {
             fill(186, 100, 11)
             ellipse(this.x - this.velocity.x * this.tracer_length, this.y - this.velocity.y * this.tracer_length, this.size, this.size)
@@ -67,5 +81,6 @@ class Star {
         }
         fill(255)
         ellipse(this.x, this.y, this.size, this.size)
+        image(pg, this.x - this.size * 5, this.y - this.size * 5, this.size * 10, this.size * 10)
     }
 }
--- scripts/stars.js
+++ scripts/stars.js
@@ -1,33 +1,46 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var max_stars = window.innerWidth * 1.5;
+var max_stars = window.innerWidth * 0.5;
 var stars = [];
 var canvas_width = document.body.clientWidth;
 var canvas_height = document.body.clientHeight;
+var pg = void 0;
 
 function setup() {
     createCanvas(canvas_width, canvas_height);
+
+    // Generate initial stars
     for (var i = 0; i < max_stars; i++) {
         stars.push(new Star());
     }
+
+    // Create star glow
+    pg = createGraphics(100, 100);
+    pg.background('rgba(0,0,0,0)');
+    pg.stroke(255);
+    pg.ellipse(50, 50, 25, 25);
+    pg.filter(BLUR, 10);
+    pg.loadPixels();
+    pg.updatePixels();
+    pg.filter(DILATE);
 }
 
 function windowResized() {
     canvas_width = document.body.clientWidth;
     canvas_height = document.body.clientHeight;
     resizeCanvas(canvas_width, canvas_height);
-    if (window.innerWidth * 1.5 > max_stars) {
+    if (window.innerWidth * 0.5 > max_stars) {
         for (var i = 0; i < max_stars; i++) {
             if (!stars[i].length) {
                 stars.push(new Star());
             }
         }
     }
-    max_stars = window.innerWidth * 1.5;
+    max_stars = window.innerWidth * 0.5;
     stars = stars.splice(0, max_stars);
 }
 
@@ -53,11 +66,11 @@ var Star = function () {
     }
 
     _createClass(Star, [{
-        key: "move",
+        key: 'move',
         value: function move() {
             var distance_to_cursor = dist(this.x, this.y, mouseX, mouseY);
-            var multiplier = !mouseIsPressed ? distance_to_cursor < 130 ? (150 - distance_to_cursor) * 0.04 : 1 : 1;
-            this.tracer_length = distance_to_cursor < 150 ? (150 - distance_to_cursor) * 0.2 : 0;
+            var multiplier = !mouseIsPressed ? distance_to_cursor < 160 ? (180 - distance_to_cursor) * 0.04 : 1 : 1;
+            this.tracer_length = distance_to_cursor < 180 ? (180 - distance_to_cursor) * 0.2 : 0;
             this.x += this.velocity.x * multiplier;
             this.y += this.velocity.y * multiplier;
             if (this.x < 0) {
@@ -72,8 +85,9 @@ var Star = function () {
             }
         }
     }, {
-        key: "draw",
+        key: 'draw',
         value: function draw() {
+            noStroke();
             if (this.tracer_length) {
                 fill(186, 100, 11);
                 ellipse(this.x - this.velocity.x * this.tracer_length, this.y - this.velocity.y * this.tracer_length, this.size, this.size);
@@ -82,6 +96,7 @@ var Star = function () {
             }
             fill(255);
             ellipse(this.x, this.y, this.size, this.size);
+            image(pg, this.x - this.size * 5, this.y - this.size * 5, this.size * 10, this.size * 10);
         }
     }]);
 
