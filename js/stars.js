5a1b9172b7d2024226a456893506bc83e206b79f
Angus Moore <angusmoore73@gmail.com>
2018-08-08 22:58:13 +1000

    Speedier stars

--- js/stars.js
+++ js/stars.js
@@ -2,7 +2,6 @@ let max_stars = window.innerWidth * 0.5
 let stars = []
 let canvas_width = document.body.clientWidth
 let canvas_height = document.body.clientHeight
-let pg
 
 function setup() {
     createCanvas(canvas_width, canvas_height)
@@ -12,15 +11,38 @@ function setup() {
         stars.push(new Star)
     }
 
+    // Create star shape
+    Star.prototype.img_star = (() => {
+        let star = createGraphics(20, 20)
+        star.background('rgba(0,0,0,0)')
+        star.stroke(255)
+        star.translate(10, 10)
+        star.beginShape()
+        star.vertex(-10, 0)
+        star.bezierVertex(-10, 0, -1, 1, 0, 10)
+        star.vertex(0, 10)
+        star.bezierVertex(0, 10, 1, 1, 10, 0)
+        star.vertex(10, 0)
+        star.bezierVertex(10, 0, 1, -1, 0, -10)
+        star.vertex(0, -10)
+        star.bezierVertex(0, -10, -1, -1, -10, 0)
+        star.vertex(-10, 0)
+        star.endShape(CLOSE)
+        return star
+    })()
+
     // Create star glow
-    pg = createGraphics(100, 100)
-    pg.background('rgba(0,0,0,0)')
-    pg.stroke(255)
-    pg.ellipse(50, 50, 25, 25)
-    pg.filter(BLUR, 10)
-    pg.loadPixels()
-    pg.updatePixels()
-    pg.filter(DILATE)
+    Star.prototype.img_glow = (() => {
+        let glow = createGraphics(100, 100)
+        glow.background('rgba(0,0,0,0)')
+        glow.stroke(255)
+        glow.ellipse(50, 50, 25, 25)
+        glow.filter(BLUR, 10)
+        glow.loadPixels()
+        glow.updatePixels()
+        glow.filter(DILATE)
+        return glow
+    })()
 }
 
 function windowResized() {
@@ -74,13 +96,12 @@ class Star {
     draw() {
         noStroke()
         if (this.tracer_length) {
+            image(this.img_glow, this.x - this.size * 5, this.y - this.size * 5, this.size * 10, this.size * 10)
             fill(186, 100, 11)
             ellipse(this.x - this.velocity.x * this.tracer_length, this.y - this.velocity.y * this.tracer_length, this.size, this.size)
             fill(200, 29, 37)
             ellipse(this.x - this.velocity.x * this.tracer_length / 2, this.y - this.velocity.y * this.tracer_length / 2, this.size, this.size)
         }
-        fill(255)
-        ellipse(this.x, this.y, this.size, this.size)
-        image(pg, this.x - this.size * 5, this.y - this.size * 5, this.size * 10, this.size * 10)
+        image(this.img_star, this.x - this.size, this.y - this.size, this.size * 2, this.size * 2)
     }
 }
--- scripts/stars.js
+++ scripts/stars.js
@@ -8,7 +8,6 @@ var max_stars = window.innerWidth * 0.5;
 var stars = [];
 var canvas_width = document.body.clientWidth;
 var canvas_height = document.body.clientHeight;
-var pg = void 0;
 
 function setup() {
     createCanvas(canvas_width, canvas_height);
@@ -18,15 +17,38 @@ function setup() {
         stars.push(new Star());
     }
 
+    // Create star shape
+    Star.prototype.img_star = function () {
+        var star = createGraphics(20, 20);
+        star.background('rgba(0,0,0,0)');
+        star.stroke(255);
+        star.translate(10, 10);
+        star.beginShape();
+        star.vertex(-10, 0);
+        star.bezierVertex(-10, 0, -1, 1, 0, 10);
+        star.vertex(0, 10);
+        star.bezierVertex(0, 10, 1, 1, 10, 0);
+        star.vertex(10, 0);
+        star.bezierVertex(10, 0, 1, -1, 0, -10);
+        star.vertex(0, -10);
+        star.bezierVertex(0, -10, -1, -1, -10, 0);
+        star.vertex(-10, 0);
+        star.endShape(CLOSE);
+        return star;
+    }();
+
     // Create star glow
-    pg = createGraphics(100, 100);
-    pg.background('rgba(0,0,0,0)');
-    pg.stroke(255);
-    pg.ellipse(50, 50, 25, 25);
-    pg.filter(BLUR, 10);
-    pg.loadPixels();
-    pg.updatePixels();
-    pg.filter(DILATE);
+    Star.prototype.img_glow = function () {
+        var glow = createGraphics(100, 100);
+        glow.background('rgba(0,0,0,0)');
+        glow.stroke(255);
+        glow.ellipse(50, 50, 25, 25);
+        glow.filter(BLUR, 10);
+        glow.loadPixels();
+        glow.updatePixels();
+        glow.filter(DILATE);
+        return glow;
+    }();
 }
 
 function windowResized() {
@@ -89,14 +111,13 @@ var Star = function () {
         value: function draw() {
             noStroke();
             if (this.tracer_length) {
+                image(this.img_glow, this.x - this.size * 5, this.y - this.size * 5, this.size * 10, this.size * 10);
                 fill(186, 100, 11);
                 ellipse(this.x - this.velocity.x * this.tracer_length, this.y - this.velocity.y * this.tracer_length, this.size, this.size);
                 fill(200, 29, 37);
                 ellipse(this.x - this.velocity.x * this.tracer_length / 2, this.y - this.velocity.y * this.tracer_length / 2, this.size, this.size);
             }
-            fill(255);
-            ellipse(this.x, this.y, this.size, this.size);
-            image(pg, this.x - this.size * 5, this.y - this.size * 5, this.size * 10, this.size * 10);
+            image(this.img_star, this.x - this.size, this.y - this.size, this.size * 2, this.size * 2);
         }
     }]);
 
